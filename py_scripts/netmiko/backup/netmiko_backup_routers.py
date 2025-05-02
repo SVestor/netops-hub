3ff9de85a61ea7a50efd0412a09a551be91be55c
SV <cloud@svestor.link>
2025-05-02 22:37:20 +0300

    add netmiko module | backup: multiple_devices_exmpl

---
+++ py_scripts/netmiko/backup/netmiko_backup_routers.py
@@ -0,0 +1,35 @@
+from netmiko import ConnectHandler
+
+with open('devices_ip.txt') as file:
+    devices = file.read().splitlines()
+
+for ip in devices:
+    cisco_router = {
+        'host': ip,
+        'username': 'admin',
+        'password': 'cisco123',
+        'device_type': 'cisco_ios',
+        'port': 22,                    #optional, default 22
+        'secret': 'cisco123',          #optional, default ''
+        'verbose': True                #optional, default False
+    }
+
+    connection = ConnectHandler(**cisco_router)
+    print('Entering enable mode...')
+    connection.enable()
+
+    output = connection.send_command('show run')
+    prompt = connection.find_prompt()
+    hostname = prompt[:-1]
+
+    filename = f'{hostname}-backup.txt'
+
+    with open(filename, 'w') as file:
+        file.write(output)
+        print(f'Backup file of {hostname} created successfully: {filename}')
+        print('#' * 50)
+    
+    print('\nClosing connection...')
+    connection.disconnect()
+
+    print('#' * 50)
